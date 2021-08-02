e983f52479d3941d08f4e894e42e6077ddd2e4f4
Vedant Deshpande <vedantnd111@gmail.com>
2021-08-02 13:08:15 +0530

    improving contact us form

--- src/User/Contactus.js
+++ src/User/Contactus.js
@@ -1,42 +1,9 @@
-// import React from 'react';
-
-// const Contactus = () => {
-//     return (
-// <div>
-//     <h3>Contactus</h3>
-//     <div>
-//         <form>
-//             <div className="form-group">
-//                 <label className="text-muted">Name</label>
-//                 <input type="text" className="form-control" />
-//             </div>
-
-//             <div className="form-group">
-//                 <label className="text-muted">Email</label>
-//                 <input type="email" className="form-control" />
-//             </div>
-
-//             <div className="form-group">
-//                 <label className="text-muted">Password</label>
-//                 <input type="password" className="form-control" />
-//             </div>
-//             <button className="btn btn-primary">
-//                 Submit
-//             </button>
-//         </form>
-//     </div>
-// </div>
-// );
-// }
-
-// export default Contactus;
-
 import React from 'react'
 
 function Contactus() {
     return (
-        <div className="container">
-            <h3 style={{ textAlign: "center" }}>Contactus</h3>
+        <div className="container" style={{ border: "2px solid black", borderRadius: "5px", alignContent: "center", justifyContent: "center", marginTop: "10px", padding: "20px" }}>
+            <h3 style={{ textAlign: "center" }}>Contact Us</h3>
             <div>
                 <form>
                     <div className="form-group">
